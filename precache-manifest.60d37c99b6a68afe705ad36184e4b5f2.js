ebef03425ee018e241882dbcced7f36d7f04d2d6
Josh Jones <ohblonddev@gmail.com>
2019-03-01 18:22:56 +0000

    Updates

---
+++ precache-manifest.60d37c99b6a68afe705ad36184e4b5f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7a660577081af120aee",
+    "url": "/connect-four/static/css/main.0e9a9f5a.chunk.css"
+  },
+  {
+    "revision": "b7a660577081af120aee",
+    "url": "/connect-four/static/js/main.b7a66057.chunk.js"
+  },
+  {
+    "revision": "09821680f0b226e7a566",
+    "url": "/connect-four/static/js/1.09821680.chunk.js"
+  },
+  {
+    "revision": "749ea7ab433c8134875f",
+    "url": "/connect-four/static/js/runtime~main.749ea7ab.js"
+  },
+  {
+    "revision": "785dfbb039370b9bf6905a87f7bcfe52",
+    "url": "/connect-four/index.html"
+  }
+];
