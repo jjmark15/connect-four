c858e4f37c1d598d22c3f73902fd44df08eb5396
Josh Jones <ohblonddev@gmail.com>
2019-03-07 23:43:14 +0000

    (wip) convert GameBoard to react hook

--- src/GameBoard/GameBoard.js
+++ src/GameBoard/GameBoard.js
@@ -1,93 +1,77 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './GameBoard.css';
 import BoardColumn from './BoardColumn';
 import Player from './GamePlayer';
 import WinnerFinder from './WinnerFinder';
 
-class GameBoard extends Component {
+function GameBoard() {
 
-  constructor() {
-    super()
+  const [nColumns, setNColumns] = useState(7);
+  const [nRows, setNRows] = useState(6);
+  const [players, setPlayers] = useState([]);
+  const [currentPlayer, setCurrentPlayer] = useState(null);
+  const [moveHistory, setMoveHistory] = useState([]);
+  const [gameState, setGameState] = useState([]);
+  const [winner, setWinner] = useState(null);
 
-    this.freshState = {
-      nColumns: 7,
-      nRows: 6,
-      players: [],
-      currentPlayer: null,
-      moveHistory: [],
-      gameState: [],
-      winner: null
-    };
+  let initialPlayerIndex = 0;
+  const playerColours = ['green', 'pink'];
+  const wf = new WinnerFinder(isCoordOnBoard);
+  let columnElements = [];
 
-    this.state = {...this.freshState};
+  setPlayers(['player1', 'player2'].map((playerName, index) => {
+    return new Player(index, playerName, playerColours[index])
+  }));
+  setCurrentPlayer(players[initialPlayerIndex])
 
-    this.initialPlayerIndex = 0;
+  useEffect(() => {
+    columnElements = generateColumnElements();
+    setGameState(buildGameState());
+  })
 
-    this.playerColours = ['green', 'pink'];
+  return (
+    <div className="Board">
+      <p>{winner ? winner.playerName : ''}</p>
+      <div className="BoardGrid" >
+        {columnElements}
+      </div>
+    </div>
+  );
 
-    this.generateColumnElements = this.generateColumnElements.bind(this);
-    this.generateTokenObjects = this.generateTokenObjects.bind(this);
-    this.generateColumnObjects = this.generateColumnObjects.bind(this);
-    this.getCurrentPlayer = this.getCurrentPlayer.bind(this);
-    this.getNextPlayerIndex = this.getNextPlayerIndex.bind(this);
-    this.endCurrentPlayerTurn = this.endCurrentPlayerTurn.bind(this);
-    this.createPlayers = this.createPlayers.bind(this);
-    this.isCoordOnBoard = this.isCoordOnBoard.bind(this);
-
-    this.WinnerFinder = new WinnerFinder(this.isCoordOnBoard);
+  function getCurrentPlayer() {
+    return currentPlayer;
   }
 
-  createPlayers(playerNames, cb=()=>{}) {
-    this.setState({
-      players: playerNames.map((playerName, index) => {
-        return new Player(index, playerName, this.playerColours[index])
-      })
-    }, cb)
+  function getPlayerById(playerId) {
+    return players[playerId]
   }
 
-  getCurrentPlayer() {
-    return this.state.currentPlayer;
-  }
-
-  getPlayerById(playerId) {
-    return this.state.players[playerId]
-  }
-
-  getNextPlayerIndex() {
-    const currentPlayerIndex = this.state.players.indexOf(this.getCurrentPlayer());
-    if (currentPlayerIndex + 1 >= this.state.players.length) {
+  function getNextPlayerIndex() {
+    const currentPlayerIndex = players.indexOf(getCurrentPlayer());
+    if (currentPlayerIndex + 1 >= players.length) {
       return 0;
     } else {
       return currentPlayerIndex + 1;
     }
   }
 
-  endCurrentPlayerTurn(move) {
-    if (this.state.winner !== null) return;
-    const newMoveHistory = [...this.state.moveHistory, move];
-    this.setState({
-      currentPlayer: this.state.players[this.getNextPlayerIndex()],
-      moveHistory: newMoveHistory,
-      gameState: this.buildGameState(newMoveHistory)
-    }, () => {
-      const lastMove = this.state.moveHistory.slice(-1)[0];
-      this.WinnerFinder.analyse(
-        this.state.gameState,
-        [lastMove.columnId, lastMove.rowIndex]);
-      if (this.WinnerFinder.winnerFound) {
-        this.setWinner(this.WinnerFinder.winner);
-      }
-    })
+  function endCurrentPlayerTurn(move) {
+    if (winner !== null) return;
+    const newMoveHistory = [...moveHistory, move];
+    setCurrentPlayer(players[getNextPlayerIndex()])
+    setMoveHistory(newMoveHistory);
+    setGameState(buildGameState(newMoveHistory));
+
+    const lastMove = moveHistory.slice(-1)[0];
+    wf.analyse(
+      gameState,
+      [lastMove.columnId, lastMove.rowIndex]);
+    if (wf.winnerFound) {
+      setWinner(wf.winner);
+    }
   }
 
-  setWinner(player, cb = () => {}) {
-    console.log(player.playerName);
-    this.setState({
-      winner: player
-    }, cb)
-  }
-
-  generateTokenObjects(column) {
+  function generateTokenObjects(column) {
     return column.map(rowSpace => {
       return {
         ...rowSpace
@@ -95,9 +79,9 @@ class GameBoard extends Component {
     })
   }
 
-  buildGameState(moveHistory = this.state.moveHistory) {
-    const newGameState = [...Array(this.state.nColumns)].map(() => {
-      return [...Array(this.state.nRows)].map((rowSpace, rowIndex) => {
+  function buildGameState(localMoveHistory = moveHistory) {
+    const newGameState = [...Array(nColumns)].map(() => {
+      return [...Array(nRows)].map((rowSpace, rowIndex) => {
         return {
           ownedBy: null,
           rowIndex
@@ -105,64 +89,41 @@ class GameBoard extends Component {
       });
     })
 
-    moveHistory.forEach(move => {
+    localMoveHistory.forEach(move => {
       newGameState[move.columnId][move.rowIndex]
-        .ownedBy = this.getPlayerById(move.playerId)
+        .ownedBy = getPlayerById(move.playerId)
     })
 
     return newGameState;
   }
 
-  generateColumnObjects() {
-    return this.state.gameState.map(column => {
+  function generateColumnObjects() {
+    return gameState.map(column => {
       return {
-        tokens: this.generateTokenObjects(column)
+        tokens: generateTokenObjects(column)
       }
     })
   }
 
-  isCoordOnBoard(columnIndex, rowIndex) {
+  function isCoordOnBoard(columnIndex, rowIndex) {
     return ![
-      columnIndex < this.state.nColumns,
-      rowIndex < this.state.nRows,
+      columnIndex < nColumns,
+      rowIndex < nRows,
       columnIndex >= 0,
       rowIndex >= 0
     ].includes(false);
   }
 
-  componentDidMount() {
-    this.setState({gameState: this.buildGameState()}, () => {
-      this.createPlayers(['player1', 'player2'], () => {
-        this.setState({
-          currentPlayer: this.state.players[this.initialPlayerIndex],
-        })
-      })
-    })
-  }
-
-  render() {
-    const columnElements = this.generateColumnElements();
-
-    return (
-      <div className="Board">
-        <p>{this.state.winner ? this.state.winner.playerName : ''}</p>
-        <div className="BoardGrid" >
-          {columnElements}
-        </div>
-      </div>
-    );
-  }
-
-  generateColumnElements() {
-    const columnObjects = this.generateColumnObjects();
+  function generateColumnElements() {
+    const columnObjects = generateColumnObjects();
 
     return columnObjects.map((col, index) => {
       return (
         <React.Fragment key={index}>
           <BoardColumn columnid={index}
             tokenobjects={col.tokens}
-            getcurrentplayer={this.getCurrentPlayer}
-            movesignal={this.endCurrentPlayerTurn} / >
+            getcurrentplayer={getCurrentPlayer}
+            movesignal={endCurrentPlayerTurn} / >
         </React.Fragment>
       )
     })
